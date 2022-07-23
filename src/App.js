f2717d7e7d0f3c4e87a3fb626742966e3f04d1b3
David Nesbitt <davenesbitt7@gmail.com>
2022-07-23 17:41:28 +0100

    initial push with all project files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LandingPage from './components/pages/LandingPage';
+import './styles/styles.scss';
+
+
+
+function App() {
+  return (
+   <LandingPage/>
+  );
+}
+
+export default App;
